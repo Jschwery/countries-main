f978c507869e1eb62681a87e1b7d632335a6a4df
Jschwery <86555316+Jschwery@users.noreply.github.com>
2023-03-07 03:17:58 -0600

    recent-countries - unstable

--- components/recentCountries.tsx
+++ components/recentCountries.tsx
@@ -4,31 +4,36 @@ import React, { useEffect, useState } from 'react'
 import { fetchCountries } from './helpers/countryFetch';
 import { Country } from '@app/page';
 import { CountryHistory } from '@global/features/countryHistory/countryHistorySlice';
+import Image from 'next/dist/client/image';
 
 
 interface CountryName {
-    name: string,
+    name?: string,
 }
-/*
- countryData.filter((country) => {
-                  const name = country.name.toString();
-                  return name.toLowerCase().includes(phrase.toLowerCase());
-                }).map((c, iteration)=>{
-                  return(
-                    <CountryLink href={`/${c.name}`}>
-                   <li key={c.name+"-"+iteration} className='hover:bg-slate-700 hover:opacity-60 p-2 leading-4 text-lg cursor-pointer h-[45px] flex align-baseline'>
-                    <Image alt='flag' width={48} height={35} className='  rounded-md px-1' src={`${c.flags.png||c.flags.svg}`}/>
-                    <div className='truncate self-center'>
-                      {c.name.length > 15 ? `${c.name.trim().slice(0,20)}...` : `${c.name}`}
-*/
 
+//  countryData.filter((country) => {
+//                   const name = country.name.toString();
+//                   return name.toLowerCase().includes(phrase.toLowerCase());
+//                 }).map((c, iteration)=>{
+//                   return(
+//                     <CountryLink href={`/${c.name}`}>
+//                    <li key={c.name+"-"+iteration} className='hover:bg-slate-700 hover:opacity-60 p-2 leading-4 text-lg cursor-pointer h-[45px] flex align-baseline'>
+//                     <Image alt='flag' width={48} height={35} className='  rounded-md px-1' src={`${c.flags.png||c.flags.svg}`}/>
+//                     <div className='truncate self-center'>
+//                       {c.name.length > 15 ? `${c.name.trim().slice(0,20)}...` : `${c.name}`}
+
+{/* 
 //[countriespage] will need to set the country name into the global state
-//then it can be used by the recent countries
+//then it can be used by the recent countries */}
 
+
+const initialState: CountryHistory = {
+  value : [['', '']]
+}
 
 async function RecentCountries({name}: CountryName) {
   const fetchedCountries = await fetchCountries();
-  const [countries, setCountries] = useState<CountryHistory[]>([[''],['']]);
+  const [countries, setCountries] = useState<CountryHistory>(initialState);
 
   useEffect(()=>{
     const foundCountry: Country | undefined = fetchedCountries.find((country) =>{
@@ -46,8 +51,11 @@ async function RecentCountries({name}: CountryName) {
 
   return (
     <div className='flex dark:bg-slate-500 max-w-md'>
-        {countries && countries.map((c, index) => (
-            <div key={`${c.name}-${index}`}>{c.name}</div>
+        {countries && countries.value.map((c, index) => (
+          <div key={`${c}-${index}`}>
+            <h3>{c[1]}</h3>
+            <img src={`${c[1]}`} alt={`${c[0]}'s flag`} />
+          </div>
         ))}
     </div>
 
