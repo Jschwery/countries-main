4746ff71f8625b047278dfdb791f86d73443623e
Jschwery <86555316+Jschwery@users.noreply.github.com>
2023-03-09 00:13:24 -0600

    context change

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,16 @@ import DarkMode from './darkMode';
 import RecentCountries from './recentCountries';
 
 
+//recent countries should have margin to center the countries within it should start at the beginning and
+//then go to the right, we will have a max width, an an overflow x-auto to scroll
+//the countries will be links that can be clicked on
 
+//on the page, have a compare countries option that will have a section from the history
+//and also from a the list of all countries
+//the page can have a component that will be displayed based off of the state of shown or unshown
+//if shown then the country page will be diplayed and shift the current country to the right.
+
+//option to save comparisons
 const Header = () => {
   return (
     <div className='p-3 flex shadow-md md:align-middle justify-between dark:bg-slate-700 shadow-black'>
--- global/provider.tsx
+++ global/provider.tsx
@@ -1,12 +1,55 @@
 'use client';
-import { ReactNode } from "react";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import {createContext,  ReactNode,  useContext, useReducer} from 'react';
+import { Country } from '@app/page';
 
-export function Providers({children}: {children: ReactNode}){
-    return(
-        <Provider store={store}>
-            {children}
-        </Provider>
-    )
+type GlobalState = {
+globalCountries: Country[],
 }
+type Action =
+  | { type: "ADD_COUNTRY"; payload: Country }
+  | { type: "REMOVE_COUNTRY"; payload: string };
+
+  const initialState: GlobalState = {
+    globalCountries: [],
+  };
+
+  function globalReducer(state: GlobalState, action: Action): GlobalState {
+    switch (action.type) {
+      case "ADD_COUNTRY":
+        return {
+          ...state,
+          globalCountries: [...state.globalCountries, action.payload],
+        };
+      case "REMOVE_COUNTRY":
+        return {
+          ...state,
+          globalCountries: state.globalCountries.filter(
+            (c) => c.name.common !== action.payload
+          ),
+        };
+      default:
+        return state;
+    }
+  }
+
+  interface GlobalContextType {
+    state: GlobalState;
+    dispatch: React.Dispatch<Action>;
+  }
+  const GlobalContext = createContext<GlobalContextType>({
+    state: initialState,
+    dispatch: () => null,
+  });
+
+  export const GlobalProvider = ({ children } : {children: ReactNode}) => {
+    const [state, dispatch] = useReducer(globalReducer, initialState);
+  
+    return (
+      <GlobalContext.Provider value={{ state, dispatch }}>
+        {children}
+      </GlobalContext.Provider>
+    );
+  };
+  export const useGlobalContext = () => useContext(GlobalContext);
+
+
