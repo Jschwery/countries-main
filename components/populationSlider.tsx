95ddecc2e32ff92cae80f73a0fe8e44b879f5d6f
Jschwery <86555316+Jschwery@users.noreply.github.com>
2023-03-11 20:03:36 -0600

    s

--- components/populationSlider.tsx
+++ components/populationSlider.tsx
@@ -1,18 +1,23 @@
 'use client';
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
+import { Slider, SliderThumb as Thumb } from '@mui/material';
 import { CheckIcon } from '@heroicons/react/24/solid';
+import { useGlobalContext } from '@global/provider';
 function valuetext(value: number) {
   return `${value}°C`;
 }
 
 const minDistance = 10;
 
+
 export default function MinimumDistanceSlider() {
 
+  const [value2, setValue2] = React.useState<number[]>([0, 15]);
 
-  const [value2, setValue2] = React.useState<number[]>([20, 37]);
+  React.useEffect(()=>{
+    console.log(value2)
+  }, [value2])
 
   const handleChange2 = (
     event: Event,
@@ -36,20 +41,40 @@ export default function MinimumDistanceSlider() {
     }
   };
 
+  // const handleCheckClick = () =>{
+     
+  // }
+
 
   return (
-    <div className='flex items-center'> 
-    <Box sx={{ width: 100%, m }}>
-      <Slider
-        getAriaLabel={() => 'Minimum distance shift'}
-        value={value2}
-        onChange={handleChange2}
-        valueLabelDisplay="auto"
-        getAriaValueText={valuetext}
-        disableSwap
+    <div className='flex items-center w-full'> 
+
+<Slider
+  className='transition-all'
+  getAriaLabel={() => 'Minimum distance shift'}
+  value={value2}
+  onChange={handleChange2}
+  valueLabelDisplay="auto"
+  getAriaValueText={valuetext}
+  disableSwap
+  sx={{ flexGrow: 1 }}
+  components={{
+    Thumb: (props) => (
+      <Thumb
+        {...props}
+        sx={{
+          width: 16,
+          height: 16,
+          '&:hover': {
+            boxShadow: '0 0 0 8px rgba(76, 175, 80, 0.16)',
+          }
+        }}
       />
-    </Box>
-    <CheckIcon className='checkApply w-[30px] h-[30px] text-green-600 hover:cursor-pointer pb-3'/>
+    )
+  }}
+/>
+{/* onClick={handleCheckClick} */}
+    <CheckIcon   className=' w-[30px] h-[30px] text-green-600 hover:text-green-400 transition-all hover:cursor-pointer pb-1 px-0.5 ml-1.5'/>
     </div>
   );
 }
